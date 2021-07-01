9a2e826b7656a55a274f2098cdc5e820c0a9f4b3
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-07-01 15:18:31 -0500

    Enlace de datos bidireccional.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,17 +1,24 @@
-describe('phoneList', function() {
+describe('phoneList', function () {
 
-    // Cargar el módulo que contiene el componente `phoneList` antes de cada prueba
-    beforeEach(module('phoneList'));
-  
-    // Prueba el controller
-    describe('PhoneListController', function() {
-  
-      it('should create a `phones` model with 3 phones', inject(function($componentController) {
-        var ctrl = $componentController('phoneList');
-  
-        expect(ctrl.phones.length).toBe(3);
-      }));
-  
+  // Cargar el módulo que contiene el componente `phoneList` antes de cada prueba
+  beforeEach(module('phoneList'));
+
+  // Prueba el controller
+  describe('PhoneListController', function () {
+    var ctrl;
+
+    beforeEach(inject(function ($componentController) {
+      ctrl = $componentController('phoneList');
+    }));
+
+    it('should create a `phones` model with 3 phones', function () {
+      expect(ctrl.phones.length).toBe(3);
     });
-  
+
+    it('should set a default value for the `orderProp` model', function () {
+      expect(ctrl.orderProp).toBe('age');
+    });
+
   });
+
+});
