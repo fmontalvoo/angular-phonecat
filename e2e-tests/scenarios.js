cf3f4777f183bf4505a757ee05f79f5243ed3028
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-07-01 16:05:43 -0500

    Plantillas de enlaces e imágenes.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -15,42 +15,49 @@ describe('PhoneCat Application', function () {
       var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
       var query = element(by.model('$ctrl.query'));
 
-      expect(phoneList.count()).toBe(3);
+      expect(phoneList.count()).toBe(20);
 
       query.sendKeys('nexus');
       expect(phoneList.count()).toBe(1);
 
       query.clear();
       query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
+      expect(phoneList.count()).toBe(8);
+    });
 
-      it('should be possible to control phone order via the drop-down menu', function () {
-        var queryField = element(by.model('$ctrl.query'));
-        var orderSelect = element(by.model('$ctrl.orderProp'));
-        var nameOption = orderSelect.element(by.css('option[value="name"]'));
-        var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+    it('should be possible to control phone order via the drop-down menu', function () {
+      var queryField = element(by.model('$ctrl.query'));
+      var orderSelect = element(by.model('$ctrl.orderProp'));
+      var nameOption = orderSelect.element(by.css('option[value="name"]'));
+      var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
 
-        function getNames() {
-          return phoneNameColumn.map(function (elem) {
-            return elem.getText();
-          });
-        }
+      function getNames() {
+        return phoneNameColumn.map(function (elem) {
+          return elem.getText();
+        });
+      }
 
-        queryField.sendKeys('tablet'); // Reduzcamos el conjunto de datos para que las afirmaciones sean más cortas
+      queryField.sendKeys('tablet'); // Reduzcamos el conjunto de datos para que las afirmaciones sean más cortas
 
-        expect(getNames()).toEqual([
-          'Motorola XOOM\u2122 with Wi-Fi',
-          'MOTOROLA XOOM\u2122'
-        ]);
+      expect(getNames()).toEqual([
+        'Motorola XOOM\u2122 with Wi-Fi',
+        'MOTOROLA XOOM\u2122'
+      ]);
 
-        nameOption.click();
+      nameOption.click();
 
-        expect(getNames()).toEqual([
-          'MOTOROLA XOOM\u2122',
-          'Motorola XOOM\u2122 with Wi-Fi'
-        ]);
-      });
+      expect(getNames()).toEqual([
+        'MOTOROLA XOOM\u2122',
+        'Motorola XOOM\u2122 with Wi-Fi'
+      ]);
+    });
 
+    it('should render phone specific links', function () {
+      var query = element(by.model('$ctrl.query'));
+      query.sendKeys('nexus');
+
+      element.all(by.css('.phones li a')).first().click();
+      expect(browser.getCurrentUrl()).toContain('index.html#!/phones/nexus-s');
     });
 
   });
