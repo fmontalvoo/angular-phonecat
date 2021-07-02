2394dbf87d50e5ace9bc77dabe1a5be21b24e05a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-07-02 11:49:13 -0500

    Filtros personalizados
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,8 @@
 angular.module('phonecatApp', [
   // ...que depende del módulo `ngRoute`.
   'ngRoute',
+  // ...que depende del módulo `core`.
+  'core',
   // ...que depende del módulo `phoneList`.
   'phoneList',
   // ...que depende del módulo `phoneDetail`.
